2210107c6f9c644c7419f4932182f40d9ddca07f
Dragboon <ye.steven@hotmail.fr>
2021-01-26 22:45:11 +0100

    Create classes WeekPlanning and WorkingHours

---
+++ JapanHotel/src/fr/esgi/planning/WeekPlanning.java
@@ -0,0 +1,11 @@
+package fr.esgi.planning;
+
+public class WeekPlanning {
+
+    private WorkingHours[] planning;
+
+    public WorkingHours getWorkingHours(String name) {
+
+    }
+
+}
---
+++ JapanHotel/src/fr/esgi/planning/WorkingHours.java
@@ -0,0 +1,14 @@
+package fr.esgi.planning;
+
+public class WorkingHours {
+
+    private String employeeName;
+    private String day;
+    private String startingTime;
+    private String endTime;
+
+    public String getEmployeeSchedule(String name) {
+
+    }
+
+}
