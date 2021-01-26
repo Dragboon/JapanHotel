f5eb7b211dabad22718a4bbf9afa8ee267322267
Dragboon <ye.steven@hotmail.fr>
2021-01-26 22:44:51 +0100

    Create classes Room and Rooms

---
+++ JapanHotel/src/fr/esgi/room/Room.java
@@ -0,0 +1,16 @@
+package fr.esgi.room;
+
+public class Room {
+
+    private int id;
+    private String theme;
+    private int beds;
+    private int nbPeople;
+    private boolean isEmpty;
+    private boolean isClean;
+
+    public void setTheme(String theme) {
+        this.theme = theme;
+    }
+
+}
---
+++ JapanHotel/src/fr/esgi/room/Rooms.java
@@ -0,0 +1,10 @@
+package fr.esgi.room;
+
+public class Rooms {
+    private Room[] rooms;
+
+    public void getRooms() {
+        this.rooms =
+    }
+
+}
